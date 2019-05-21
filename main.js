b0e832d9d265014c5b95b975be4fb554fb9b2c46
lisgroup <407505297@qq.com>
2019-05-21 17:57:07 +0800

    基本页面编辑

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 import Vue from 'vue'
 import App from './App'
 
+import pageHead from './components/page-head.vue'
+import pageFoot from './components/page-foot.vue'
+
 import store from './store'
 
 Vue.config.productionTip = false
 
 Vue.prototype.$store = store
+
+Vue.component('page-head', pageHead)
+Vue.component('page-foot', pageFoot)
 
 App.mpType = 'app'
 
--- pages/local/local.vue
+++ pages/local/local.vue
@@ -1,6 +1,17 @@
 <template>
-	<view>
-		本地生活，敬请期待。
+	<view class="page">
+		<page-head :title="title"></page-head>
+		<view class="uni-padding-wrap">
+			<view class="uni-helllo-text">
+				页面开发中。。。
+			</view>
+			<view class="uni-btn-v">
+				<button type="default" @click="setText">改变标题栏文字</button>
+				<!-- #ifndef MP-TOUTIAO -->
+				<button type="primary" @click="setBg">改变标题栏颜色</button>
+				<!-- #endif -->
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -8,15 +19,31 @@
 	export default {
 		data() {
 			return {
-				
+				title: '本地生活',
+				hasSetText:false,
+				hasSetBg:true
 			}
 		},
 		methods: {
-			
+			setText() {
+				this.hasSetText = !this.hasSetText;
+				uni.setNavigationBarTitle({
+					title: this.hasSetText ? "Hello APP" : "本地生活"
+				})
+			},
+			setBg() {
+				this.hasSetBg = !this.hasSetBg;
+				uni.setNavigationBarColor({
+					frontColor: this.hasSetBg ? "#ffffff" : "#000000",
+					backgroundColor: this.hasSetBg ? "#0FAEFF" : "#F8F8F8"
+				})
+			}
 		}
 	}
 </script>
 
 <style>
-
+	.uni-helllo-text {
+		text-align: center;
+	}
 </style>
